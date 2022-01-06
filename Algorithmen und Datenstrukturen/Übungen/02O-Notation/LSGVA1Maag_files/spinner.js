e4778d5dde4fcaa8428f03693e54b04bdd1cd022
= <mex128@outlook.com>
2022-01-06 16:40:13 +0100

    init 2. Semester

---
+++ Algorithmen und Datenstrukturen/Übungen/02O-Notation/LSGVA1Maag_files/spinner.js
@@ -0,0 +1,8 @@
+function startSpinner() { 
+    document.getElementById("loader").style.display = "inline";
+}
+function stopSpinner() { 
+    document.getElementById("loader").style.display = "none";
+}
+
+function alarmiere() {alert("VORSICHT");} 
